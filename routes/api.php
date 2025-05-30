74a5ef112cc7377ce8ab0ee90692ecf248e69438
raethrahma <dhiorahmaputra05@gmail.com>
2025-05-30 16:55:16 +0700

    API Kategori Admin

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProductApiController;
+use App\Http\Controllers\Admin\Api\CategoryController;
 use App\Http\Controllers\Auth\AuthController;
+
 
 // Auth routes
 Route::post('/register', [AuthController::class, 'register']);
@@ -17,4 +19,9 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::apiResource('products', ProductApiController::class)->except(['index', 'show']);
     Route::get('/user', [AuthController::class, 'user']); // Get authenticated user
+});
+
+// Category routes
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('categories', CategoryController::class);
 });
